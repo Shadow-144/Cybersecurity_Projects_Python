69f0eaa982a6919bb36443ae8b12026f846bc6c8
Alosh Pariyar <aloshpariyar.07@gmail.com>
2025-02-07 21:04:50 +0545

    update code

--- Password_strength_checker.py
+++ Password_strength_checker.py
@@ -1,37 +1,44 @@
-# Taking password input 
-password = input("Enter your password: ")
+import re
 
-# defining variables 
-list_of_special_characters = '''"'!@#$%^&*()-=_+,./<>?;:'''
-list_of_numbers = '''1234567890'''
-u = 0 # for upper case 
-l = 0 # for lower case 
-s = 0 # for special characters
-n = 0 # for number
+def display_result(u,l,s,n):
+    if(u>0 and l > 0 and s > 0 and n > 0):
+        print("Your password is strong and you can use it for anything")
+
+# defining variables
+u = 0
+l = 0
+s = 0
+n = 0 
+
+# Taking password input 
+password = str(input("Enter your password: "))
+
 # Checking the length of the password
-if(len(password) > 8):
+if(len(password) < 8):
     print("Your password must contain at least 8 characters")
 
-# Checking for Lower and upper characters
-for i in range(1, len(password)):
-    if(str(password[i]) == password.isupper()):
-        u = u + 1
-    if(str(password[i]) == password.islower()):
-        l = l + 1
+# Checking for Uppercase characters 
+if not (re.search(r"[A-Z]",password)):
+    print("Your passowrd must contain at least one Uppercase charcter")
+else:
+    u = u + 1
 
-# Checking for special characters
-for i in range(0, len(password)):
-    if(str(password[i]) == list_of_special_characters[i]):
-        s = s + 1 
+# Checking for lowercase characters 
+if not (re.search(r"[a-z]",password)):
+    print("Your password must contain at least one lowercase character")
+else:
+    l = l + 1
+
+# Checking for special characters 
+if not re.search(r"[{!@#$%^&*-=_+,./<>?;':\"\\|}]", password):
+    print("your password must contain at least one special character")
+else:
+    s = s + 1
 
 # Checking for numbers 
-for i in range(1, len(password)):
-    if(str(password[i]) == list_of_numbers[i]):
-        n = n + 1 
-    
-# defining function for displaying password
-def display_password(u, l , s):
-    if(u > 0 and l > 0 and s > 0):
-        print("Your password is strong")
-    else:
-        print("Your password doesnot satisfy all the conditions")
+if not re.search(r"\d", password):
+    print("your password must contain at least one number")
+else:
+    n = n + 1
+
+display_result(u,l,s,n)
